b5093f2ff3994438532440c62964c45e2a09d645
Tyrant117 <nslange11@gmail.com>
2025-12-20 15:56:57 -0800

    Fix Build Errors

--- Runtime/Keys/Generation/KeyUtility.cs
+++ Runtime/Keys/Generation/KeyUtility.cs
@@ -43,21 +43,63 @@ namespace Vapor.Keys
             s_Cached = true;
             Debug.Log($"{TooltipMarkup.ClassMethod(nameof(KeyUtility), nameof(InitEditor))} - Cached Keys");
         }
-#endif
+        #endif
+
+        private static bool s_RuntimeCached;
+        private static void InitRuntime()
+        {
+            if (s_RuntimeCached)
+            {
+                return;
+            }
+            s_CachedFieldInfos.Clear();
+            s_CachedCategories.Clear();
+            s_CachedTypeNames.Clear();
+
+            var assemblies = AppDomain.CurrentDomain.GetAssemblies();
+            foreach (var assembly in assemblies)
+            {
+                foreach (var type in assembly.GetTypes())
+                {
+                    if (typeof(IKeysProvider).IsAssignableFrom(type) && !type.IsInterface)
+                    {
+                        s_CachedFieldInfos.TryAdd(type, GetFieldInfo(type));
+                        var category = GetKeyCategory(type);
+                        if (!s_CachedCategories.TryGetValue(category, out var typeSet))
+                        {
+                            typeSet = new HashSet<Type>();
+                            s_CachedCategories[category] = typeSet;
+                        }
+                        if (!s_CachedTypeNames.TryGetValue(type.Name, out var nameSet))
+                        {
+                            nameSet = new HashSet<Type>();
+                            s_CachedTypeNames[type.Name] = nameSet;
+                        }
+                        typeSet.Add(type);
+                        nameSet.Add(type);
+                    }
+                }
+            }
+            s_RuntimeCached = true;
+            Debug.Log($"{TooltipMarkup.ClassMethod(nameof(KeyUtility), nameof(InitRuntime))} - Cached Keys");
+        }
 
         public const string ASSEMBLY_NAME = "VaporKeyDefinitions";        
         private static readonly Dictionary<Type, FieldInfo> s_CachedFieldInfos = new();
         private static readonly Dictionary<string, HashSet<Type>> s_CachedCategories = new();
         private static readonly Dictionary<string, HashSet<Type>> s_CachedTypeNames = new();
-
+        
         public static List<DropdownModel> GetAllKeysFromTypeName(string typeName)
         {
 #if UNITY_EDITOR
             InitEditor();
-            if (s_CachedTypeNames.ContainsKey(typeName))
+#else
+            InitRuntime();
+#endif
+            if (s_CachedTypeNames.TryGetValue(typeName, out var name))
             {
                 List<DropdownModel> result = new();
-                foreach (var t in s_CachedTypeNames[typeName])
+                foreach (var t in name)
                 {
                     if(s_CachedFieldInfos.TryGetValue(t, out var fi))
                     {
@@ -66,19 +108,17 @@ namespace Vapor.Keys
                 }
                 return result;
             }
-            else
-            {
-                return new List<DropdownModel>() { new ("None", KeyDropdownValue.None, "None") };
-            }
-#else
-            return new List<DropdownModel>() { new ("None", KeyDropdownValue.None) };
-#endif
+
+            return new List<DropdownModel>() { new ("None", KeyDropdownValue.None, "None") };
         }
 
         public static List<DropdownModel> GetAllKeysFromCategory(string category)
         {
 #if UNITY_EDITOR
             InitEditor();
+#else
+            InitRuntime();
+#endif
             if (s_CachedCategories.TryGetValue(category, out var cachedCategory))
             {
                 List<DropdownModel> result = new();
@@ -91,26 +131,17 @@ namespace Vapor.Keys
                 }
                 return result;
             }
-            else
-            {
-                return new List<DropdownModel>() { new ("None", KeyDropdownValue.None, "None") };
-            }
-#else
-            return new List<DropdownModel>() { new ("None", KeyDropdownValue.None) };
-#endif
+
+            return new List<DropdownModel>() { new ("None", KeyDropdownValue.None, "None") };
         }
 
         public static List<DropdownModel> GetAllKeysFromType(Type type)
         {
-#if UNITY_EDITOR
             return ((List<(string, KeyDropdownValue)>)type.GetField(KeyGenerator.KEYS_FIELD_NAME, BindingFlags.Public | BindingFlags.Static)?.GetValue(null) ?? new List<(string, KeyDropdownValue)>()
                 {
                     ("None", KeyDropdownValue.None),
                 })
                 .Select(t => new DropdownModel(t.Item1,t.Item2, t.Item1)).ToList();
-#else
-            return new List<DropdownModel>() { new ("None", KeyDropdownValue.None) };
-#endif
         }
 
         private static List<DropdownModel> GetReflectedKeys(FieldInfo fieldInfo)
@@ -132,16 +163,16 @@ namespace Vapor.Keys
             return type.GetField(KeyGenerator.KEYS_FIELD_NAME, BindingFlags.Public | BindingFlags.Static);
         }
 
+#if UNITY_EDITOR
         public static void GenerateKeysOfType<T>() where T : KeySo
         {
-#if UNITY_EDITOR
             var scriptName = typeof(T).Name;
             scriptName = scriptName.Replace("Scriptable", "");
             scriptName = scriptName.Replace("SO", "");
             scriptName = scriptName.Replace("So", "");
             scriptName = scriptName.Replace("Key", "");
             KeyGenerator.GenerateKeys(typeof(T), $"{scriptName}Keys");
-#endif
         }
+#endif
     }
 }
