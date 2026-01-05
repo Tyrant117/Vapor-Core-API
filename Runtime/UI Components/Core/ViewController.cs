c38f71a24ee4e17be78e413b22955871dd6dcc26
Tyrant117 <nslange11@gmail.com>
2026-01-05 00:57:07 -0800

    Bug Fixes In GameMode Startup

--- Runtime/UI Components/Core/ViewController.cs
+++ Runtime/UI Components/Core/ViewController.cs
@@ -5,6 +5,10 @@ namespace Vapor
 {
     public abstract class ViewController : VisualElement
     {
+        /// <summary>
+        /// Called after the rootVisualElement has been initialized and all default elements constructed.
+        /// </summary>
+        /// <param name="initializer">The object calling this method, typically the PlayerController</param>
         public abstract void OnInitialized(object initializer);
     }
 }
