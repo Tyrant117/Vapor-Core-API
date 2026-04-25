766a15c12e9d1e84a0f5caec7abfab441ddfc3bc
Tyrant117 <nslange11@gmail.com>
2026-04-24 23:09:59 -0700

    Mostly just deserialization helper changes

--- Runtime/Data Registry/AddressableData.cs
+++ Runtime/Data Registry/AddressableData.cs
@@ -1,3 +1,4 @@
+using UnityEngine;
 using UnityEngine.AddressableAssets;
 using UnityEngine.ResourceManagement.AsyncOperations;
 using Vapor.Unsafe;
@@ -37,6 +38,43 @@ namespace Vapor
             }
 
             handle = Addressables.LoadAssetAsync<T>(AddressableName);
+        }
+        
+        // GameObjects
+        public GameObject Instantiate(Vector3 position, Quaternion rotation)
+        {
+            InstantiateAsync(position, rotation, out var handle);
+            handle.WaitForCompletion();
+            return handle.Result;
+        }
+        
+        public GameObject Instantiate(Transform parent, bool instantiateInWorldSpace)
+        {
+            InstantiateAsync(parent, instantiateInWorldSpace, out var handle);
+            handle.WaitForCompletion();
+            return handle.Result;
+        }
+        
+        public void InstantiateAsync(Vector3 position, Quaternion rotation, out AsyncOperationHandle<GameObject> handle)
+        {
+            if (AddressableName.EmptyOrNull())
+            {
+                handle = default;
+                return;
+            }
+
+            handle = Addressables.InstantiateAsync(AddressableName, position, rotation);
+        }
+        
+        public void InstantiateAsync(Transform parent, bool instantiateInWorldSpace, out AsyncOperationHandle<GameObject> handle)
+        {
+            if (AddressableName.EmptyOrNull())
+            {
+                handle = default;
+                return;
+            }
+
+            handle = Addressables.InstantiateAsync(AddressableName, parent, instantiateInWorldSpace);
         }
     }
 }
