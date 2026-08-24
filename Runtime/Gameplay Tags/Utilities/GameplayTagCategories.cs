97237841c76bc2b7642f6ee680c34a753d4d755e
Tyrant117 <nslange11@gmail.com>
2026-08-24 15:30:40 -0700

    Vapor Xr Base

--- Runtime/Gameplay Tags/Utilities/GameplayTagCategories.cs
+++ Runtime/Gameplay Tags/Utilities/GameplayTagCategories.cs
@@ -21,10 +21,8 @@ namespace Vapor.GameplayTags
         Loot,
         GameplayTagData,
         Item,
+        Xr,
         Vapor,
-        WorldSettings,
-        Subsystems,
-        Actors,
     }
 
     public static class GameplayTagCategories
@@ -45,10 +43,8 @@ namespace Vapor.GameplayTags
         public const string LOOT = "Loot";
         public const string GAMEPLAY_TAG_DATA = "GameplayTagData";
         public const string ITEM = "Item";
+        public const string XR = "Xr";
         public const string VAPOR = "Vapor";
-        public const string WORLD_SETTINGS = "WorldSettings";
-        public const string SUBSYSTEMS = "Subsystems";
-        public const string ACTORS = "Actors";
 
         public static string GetNameForCategory(GameplayTagDefaultCategories category)
         {
@@ -70,10 +66,8 @@ namespace Vapor.GameplayTags
                 GameplayTagDefaultCategories.Loot => LOOT,
                 GameplayTagDefaultCategories.GameplayTagData => GAMEPLAY_TAG_DATA,
                 GameplayTagDefaultCategories.Item => ITEM,
+                GameplayTagDefaultCategories.Xr => XR,
                 GameplayTagDefaultCategories.Vapor => VAPOR,
-                GameplayTagDefaultCategories.WorldSettings => WORLD_SETTINGS,
-                GameplayTagDefaultCategories.Subsystems => SUBSYSTEMS,
-                GameplayTagDefaultCategories.Actors => ACTORS,
                 _ => throw new ArgumentOutOfRangeException(nameof(category), category, null)
             };
         }
